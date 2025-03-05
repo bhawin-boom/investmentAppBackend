82b6803733d8ef8e0d2e097dec06800b59fbc40c
bhawin-boom <bhawin.parkeria@gmail.com>
2025-03-05 21:55:24 +0530

    Initial commit

---
+++ src/main/java/com/example/Bhawin/BhawinApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bhawin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhawinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhawinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Bhawin/BhawinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bhawin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhawinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
